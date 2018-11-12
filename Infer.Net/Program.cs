1274dd00e58a94c571c0d32e1858d8092980f0be
Vladimir Shchur <shchurvladimir@gmail.com>
2018-11-12 21:00:42 +0300

    Added 4th variable

--- Infer.Net/Program.cs
+++ Infer.Net/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.ML.Probabilistic.Models;
+using Microsoft.ML.Probabilistic.Models.Attributes;
 using System;
 
 namespace Infer.Net
@@ -7,28 +8,36 @@ namespace Infer.Net
     {
         static void Main(string[] args)
         {
-            Variable<bool> knowsCsharp = Variable.Bernoulli(0.5);
-            Variable<bool> knowsSql = Variable.Bernoulli(0.5);
+            Variable<bool> knowsCsharp = Variable.Bernoulli(0.5).Attrib(new TraceMessages());
+            Variable<bool> knowsSql = Variable.Bernoulli(0.5).Attrib(new TraceMessages());
             Variable<bool> knowsBoth = knowsCsharp & knowsSql;
-            // sprinkler
+
             Variable<bool> firstQuestion = Variable.New<bool>();
             using (Variable.If(knowsCsharp)) firstQuestion.SetTo(Variable.Bernoulli(0.9));
             using (Variable.IfNot(knowsCsharp)) firstQuestion.SetTo(Variable.Bernoulli(0.2));
-            // rain
+
             Variable<bool> secondQuestion = Variable.New<bool>();
             using (Variable.If(knowsSql)) secondQuestion.SetTo(Variable.Bernoulli(0.9));
             using (Variable.IfNot(knowsSql)) secondQuestion.SetTo(Variable.Bernoulli(0.2));
-            // wet grass
+
             Variable<bool> thirdQuestion = Variable.New<bool>();
             using (Variable.If(knowsBoth)) thirdQuestion.SetTo(Variable.Bernoulli(0.9));
             using (Variable.IfNot(knowsBoth)) thirdQuestion.SetTo(Variable.Bernoulli(0.2));
 
+            Variable<bool> fourthQuestion = Variable.New<bool>();
+            using (Variable.If(knowsBoth)) fourthQuestion.SetTo(Variable.Bernoulli(0.9));
+            using (Variable.IfNot(knowsBoth)) fourthQuestion.SetTo(Variable.Bernoulli(0.2));
+
             // Observations and inference
-            firstQuestion.ObservedValue = false;
+            firstQuestion.ObservedValue = true;
             secondQuestion.ObservedValue = false;
-            thirdQuestion.ObservedValue = true;
+            thirdQuestion.ObservedValue = false;
+            fourthQuestion.ObservedValue = false;
             InferenceEngine ie = new InferenceEngine();
-            ie.ShowProgress = false;
+            ie.ShowTimings = true;
+            ie.ShowProgress = true;
+            ie.ShowSchedule = true;
+            ie.ShowFactorGraph = true;
             Console.WriteLine("P(csharp)=" + ie.Infer(knowsCsharp));
             Console.WriteLine("P(sql)=" + ie.Infer(knowsSql));
         }
